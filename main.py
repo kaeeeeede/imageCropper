1721f2b7492cda4510584ea68d4ef82302ac1296
AntWong-TheBadNomads <86905375+AntWong-TheBadNomads@users.noreply.github.com>
2023-06-18 00:10:41 +0800

    loading image

---
+++ main.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+from PIL import Image
+
+image_path = r'test.png'
+
+layout = [[sg.Graph(
+    canvas_size=(400, 400),
+    graph_bottom_left=(0, 0),
+    graph_top_right=(400, 400),
+    key="-GRAPH-",
+    change_submits=True,  # mouse click events
+    drag_submits=True), ],
+    [sg.Text(key='info', size=(60, 1))]]
+
+window = sg.Window("Cropper", layout, finalize=True)
+graph = window["-GRAPH-"]
+
+def load_image_on_graph(graph, image_path):
+	im = Image.open(image_path)
+	graph.draw_image(image_path, location=(0,400))
+	graph.set_size(im.size)
+
+load_image_on_graph(graph, "test.png")
+
+while True:
+    event, values = window.read()
+    
+    if event == sg.WIN_CLOSED:
+        break  # exit
