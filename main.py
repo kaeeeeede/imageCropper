91ea650265a89c60ca657e7f368ee6fd846db292
kaeeeeede <kaedewrites1@gmail.com>
2023-06-18 00:04:09 +0800

    completed crop function

---
+++ main.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+def crop_image(image, x_offset, y_offset, width, height):
+	crop_box = (x_offset, y_offset, width + x_offset, height + y_offset)
+	
+	result = image.crop(crop_box)
+
+	result.show()
+
+	return
+
+if __name__ == "__main__":
+	im = Image.open(r"C:\Users\Kaede\OneDrive - mmu.edu.my\Documents\Projects\imageCropper\test.jpg")
+
+	crop_image(im, 123, 165, 1230, 1860)
